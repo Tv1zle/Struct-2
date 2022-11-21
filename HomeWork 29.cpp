6d9941066d90f9d960895f6ff78179c4fbea1b74
User <User@WIN-D6I7BGDFHRA>
2022-11-21 17:53:46 +0100

    Добавьте файлы проекта.

---
+++ HomeWork 29.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+
+
+struct Device
+{
+    std::string DeviceType;
+    std::string Denomination;
+    std::string Model;
+    std::string Brand;
+    double GuaranteePeriod;
+    double Price;
+    std::string DateOfSale;
+};
+
+
+Device FillDevice()
+{
+    Device temp;
+    std::cout << "Please, enter device type:";
+    std::cin >> temp.DeviceType;
+    std::cout << "Please, enter denomination:";
+    std::cin >> temp.Denomination;
+    std::cout << "Please, enter model:";
+    std::cin >> temp.Model;
+    std::cout << "Please, enter brand:";
+    std::cin >> temp.Brand;
+    std::cout << "Please, enter guarantee period:";
+    std::cin >> temp.GuaranteePeriod;
+    std::cout << "Please, enter price:";
+    std::cin >> temp.Price;
+    std::cout << "Please, enter date of sale:";
+    std::cin >> temp.DateOfSale;
+
+
+    return temp;
+}
+
+
+void ShowDevice(const Device& device)
+{
+    std::cout << "Device type - " << device.DeviceType << "\n";
+    std::cout << "Denomination - " << device.Denomination << "\n";
+    std::cout << "Model - " << device.Model << "\n";
+    std::cout << "Brand - " << device.Brand << "\n";
+    std::cout << "Guarantee period - " << device.GuaranteePeriod << "\n";
+    std::cout << "Price - " << device.Price << "\n";
+    std::cout << "Date of sale - " << device.DateOfSale << "\n";
+}
+
+
+int main()
+{
+    Device device = FillDevice();
+    ShowDevice(device);
+}
